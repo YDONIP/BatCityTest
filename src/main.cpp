544e8d1103fac9665be6b196d7d3600460ee684a
zomgra <hiban.com@gmail.com>
2020-07-29 19:56:34 +0300

    Add OPenGL2.X

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,45 @@
-#include<iostream>
-int main  (int argc, char* argv[]){
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+int main(void)
+{
+    GLFWwindow* window;
 
-    std::cout << "Hello World" << std::endl;
-    return 0 ;
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    if(!gladLoadGL()){
+        std::cout<<"ERROR LOAD GLAD"<<std::endl;
+        return -1;
+    }
+    std::cout << "OpenGL"<< GLVersion.major << "." << GLVersion.minor<< std::endl;
+    /* Make the window's context current */
+    glClearColor(0, 1, 0, 1);
+    
+
+    
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
 }
-
