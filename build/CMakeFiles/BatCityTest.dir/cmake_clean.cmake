3eb657133fc7c7f0f732545e2c7d72531697a88d
zomgra <hiban.com@gmail.com>
2020-07-30 08:08:19 +0300

    Added build/

---
+++ build/CMakeFiles/BatCityTest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "BatCityTest"
+  "BatCityTest.pdb"
+  "CMakeFiles/BatCityTest.dir/src/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BatCityTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
